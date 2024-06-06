8ef084d9698903bb6fa5ca071dde7085a87af31f
AshClasse <classeashley@gmail.com>
2024-06-06 16:46:23 -0400

    Add project files.

---
+++ RealEstateApp.Infrastructure.Persistence/Repositories/PropertyRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RealEstateApp.Core.Application.Interfaces.Repositories;
+using RealEstateApp.Core.Domain.Entities;
+using RealEstateApp.Infrastructure.Persistence.Contexts;
+using RealEstateApp.Infrastructure.Persistence.Repositories.Generic;
+
+namespace RealEstateApp.Infrastructure.Persistence.Repositories
+{
+    public class PropertyRepository : GenericRepository<Property>, IPropertyRepository
+    {
+        private readonly ApplicationContext _dbContext;
+
+        public PropertyRepository(ApplicationContext dbContext) : base(dbContext) 
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Property>> GetAllWithIncludeAsync(List<string> properties)
+        {
+            var query = _dbContext.Set<Property>().AsQueryable();
+
+            foreach (string property in properties)
+            {
+                query = query.Include(property);
+            }
+
+            return await query.Where(q => !q.IsDeleted).ToListAsync();
+        }
+    }
+}
